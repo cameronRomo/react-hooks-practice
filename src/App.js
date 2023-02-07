c6eaf081eda6665f0f51213db978cb436af41d87
Cameron Romo <cromo1225@gmail.com>
2023-02-07 08:45:44 -0700

    Using useContext with class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
-import Movie from "./hoc/Movie";
-import Counter from "./hooks/Counter";
-import Users  from "./hooks/Users";
-import "./App.css";
+import React, { Component } from "react";
+import MoviePage from "./context/MoviePage";
+import UserContext from "./context/userContext";
 
-function App() {
-  return <Users />;
+class App extends Component {
+  state = { currentUser: { name: "Cameron" } };
+
+  render() {
+    return (
+      <UserContext.Provider value={this.state.currentUser}>
+        <div>
+          <MoviePage />
+        </div>
+      </UserContext.Provider>
+    );
+  }
 }
 
 export default App;
